469f7d863224fcb5fc4b0572ab8ed642cfbcd3f7
Sugar_F0x <malion.torchwood@yandex.ru>
2020-07-23 21:20:15 +0300

    :white_check_mark: Set up storage
    > Run storage update on page load
    > Add mutation that commits to both vuex.state storage and localStorage
    > Add dummy (commented out) segment of dummy data for testing purposes

--- src/main.js
+++ src/main.js
@@ -8,5 +8,64 @@ Vue.config.productionTip = false;
 new Vue({
   router,
   store,
+  mounted() {
+    let stickers = JSON.parse(localStorage.getItem('stickers')) || [];
+    this.$store.commit('setStickers', stickers);
+  },
   render: h => h(App)
 }).$mount('#app');
+
+// const gay =
+// JSON.stringify(
+//   [
+//     {
+//       title: 'Dummy title 1',
+//       tasks: [
+//         {
+//           state: false,
+//           text:  'Dummy checkbox 1'
+//         },
+//         {
+//           state: false,
+//           text:  'Dummy checkbox 2'
+//         },
+//         {
+//           state: false,
+//           text:  'Dummy checkbox 3'
+//         }
+//       ]
+//     },{
+//       title: 'Dummy title 2',
+//       tasks: [
+//         {
+//           state: false,
+//           text:  'Dummy checkbox 1'
+//         },
+//         {
+//           state: false,
+//           text:  'Dummy checkbox 2'
+//         },
+//         {
+//           state: false,
+//           text:  'Dummy checkbox 3'
+//         }
+//       ]
+//     },{
+//       title: 'Dummy title 3',
+//       tasks: [
+//         {
+//           state: false,
+//           text:  'Dummy checkbox 1'
+//         },
+//         {
+//           state: false,
+//           text:  'Dummy checkbox 2'
+//         },
+//         {
+//           state: false,
+//           text:  'Dummy checkbox 3'
+//         }
+//       ]
+//     },
+//   ]
+// );
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,17 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    stickers: []
   },
   mutations: {
+    setStickers(state, payload) {
+      state.stickers = payload;
+      localStorage.setItem('stickers', JSON.stringify(payload));
+    }
   },
   actions: {
   },
