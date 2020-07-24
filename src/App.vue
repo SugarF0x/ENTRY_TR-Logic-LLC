cc4f778cbfe68cd62cdb10eaa25caa43f53f0b84
Sugar_F0x <malion.torchwood@yandex.ru>
2020-07-24 14:48:02 +0300

    :white_check_mark: Add fill dummy data button for testing purposes

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,7 @@
         by
           <span class="cyan">Sugar_F0x</span>
       </h1>
+      <button @click="fillDummyData">Fill dummy data</button>
     </div>
     <router-view/>
   </div>
@@ -22,7 +23,82 @@
    * @namespace app
    */
   export default {
-    name: 'App'
+    name: 'App',
+    data() {
+      return {
+        dummyData: [
+          {
+            title: 'Dummy title 1',
+            id: '1',
+            tasks: [
+              {
+                state: false,
+                text:  'Dummy checkbox 1'
+              },
+              {
+                state: false,
+                text:  'Dummy checkbox 2'
+              },
+              {
+                state: false,
+                text:  'Dummy checkbox 3'
+              }
+            ]
+          },{
+            title: 'Dummy title 2',
+            id: '2',
+            tasks: [
+              {
+                state: false,
+                text:  'Other dummy title 1'
+              },
+              {
+                state: false,
+                text:  'Dummy checkbox 27'
+              },
+              {
+                state: false,
+                text:  'Checkbox of other length'
+              }
+            ]
+          },{
+            title: 'ASD asdafasdfasdf',
+            id: '3',
+            tasks: [
+              {
+                state: false,
+                text:  'asdfasdfsdafasdg'
+              },
+              {
+                state: false,
+                text:  'sdg'
+              },
+              {
+                state: false,
+                text:  'asdasdfasg'
+              },
+              {
+                state: false,
+                text:  'Hidden checkbox 1'
+              },
+              {
+                state: false,
+                text:  'Hidden checkbox 2'
+              },
+              {
+                state: false,
+                text:  'Hidden checkbox 3'
+              }
+            ]
+          },
+        ]
+      }
+    },
+    methods: {
+      fillDummyData() {
+        this.$store.commit('setStickers', this.dummyData)
+      }
+    }
   }
 </script>
 
@@ -43,6 +119,11 @@
     color: white;
     .cyan {
       color: cyan;
+    }
+    button {
+      position: fixed;
+      top: 1rem;
+      right: 1rem;
     }
   }
 </style>
